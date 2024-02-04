630ad049d66d4b95ed7314243f7f5e549865aa9c
Greg Johnston <gregmj.sco@gmail.com>
2024-02-04 17:42:33 +0900

    Create Header compontent with css and add boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Header/Header'
 
 function App() {
 
   return (
     <>
-     <h1>Test</h1>
+     <Header></Header>
     </>
   )
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return <header>
+        <h1>Tekken 8</h1>
+        <nav>
+            <a href="">Settings</a><a href="">Info</a><a href="">Notes</a>
+        </nav>
+    </header>
+}
+
+export default Header

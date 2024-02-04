220343f5ec48c91063bca86c1a768d74bbdb1bfe
Greg Johnston <gregmj.sco@gmail.com>
2024-02-04 17:57:40 +0900

    Update css for header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,8 @@
+import './Header.css'
+
 function Header(){
     return <header>
-        <h1>Tekken 8</h1>
+        <h1 className='title'>Tekken 8</h1>
         <nav>
             <a href="">Settings</a><a href="">Info</a><a href="">Notes</a>
         </nav>
